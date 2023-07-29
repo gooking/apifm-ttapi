73e8cfa47580de0a24fcbbce441b23bfda240402
gooking <javazj@qq.com>
2023-07-29 10:20:52 +0800

    2023-07-29

--- src/index.js
+++ src/index.js
@@ -279,6 +279,12 @@ module.exports = {
   alipayQrcode2: (data) => {
     return request('/pay/alipay/gate/paymentCode', true, 'post', data)
   },
+  kasipayH5: (data) => {
+    return request('/pay/kasipay/h5', true, 'post', data)
+  },
+  hmpayJsapi: (data) => {
+    return request('/pay/sandpay/hmpay/jsapi', true, 'post', data)
+  },
   login_wx: (code) => {
     return request('/user/wxapp/login', true, 'post', {
       code,
@@ -2193,5 +2199,8 @@ module.exports = {
   },
   communityOrderFahuo: data => {
     return request('/communityOrder/fahuo', true, 'post', data)
+  },
+  wxmpOpenid: code => {
+    return request('/user/wxmp/openid', true, 'get', { code })
   },
 }
