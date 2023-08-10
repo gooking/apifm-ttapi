59a4fefdbbd775729ff6f787a98891373b45f785
gooking <javazj@qq.com>
2023-08-10 20:06:15 +0800

    2023-08-10

--- src/index.js
+++ src/index.js
@@ -1314,6 +1314,9 @@ module.exports = {
   scoreDeductionRules: (type = '') => {
     return request('/score/deduction/rules', true, 'get', { type })
   },
+  scoreDailyFixedNum: token => {
+    return request('/score/dailyFixedNum', true, 'post', { token })
+  },
   voteItems: (data) => {
     return request('/vote/items', true, 'post', data)
   },
